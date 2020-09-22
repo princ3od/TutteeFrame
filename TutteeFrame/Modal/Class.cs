35f572dd06266d4333565a5d59c50f92d76a674d
Bình Trọng <princ3od@gmail.com>
2020-09-22 21:00:34 +0700

    Setting Up
    - Thêm vài thứ cơ bản cho project.
    - Chia folder theo mô hình MVC.
    - Cơ bản đầy đủ các lớp data-structure cần có.

---
+++ TutteeFrame/Modal/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TutteeFrame.Modal
+{
+    class Class
+    {
+        private string iD;
+        private string room;
+        private int studentNum;
+        private string formerTeacherID;
+
+        public string ID { get => iD; set => iD = value; }
+        public string Room { get => room; set => room = value; }
+        public int StudentNum { get => studentNum; set => studentNum = value; }
+        public string FormerTeacherID { get => formerTeacherID; set => formerTeacherID = value; }
+    }
+}
