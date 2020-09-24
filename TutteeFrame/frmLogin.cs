bdfda3e53f2faf9b5bb4cb0d0757a4b400bedd07
Bình Trọng <princ3od@gmail.com>
2020-09-24 11:10:58 +0700

    Add UI Library
    - Thêm 2 thư viện để thiết kế UI.
    - Thêm form đăng nhập.

---
+++ TutteeFrame/frmLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace TutteeFrame
+{
+    public partial class frmLogin : MetroForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
