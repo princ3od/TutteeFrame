cbb244ee124c43050bf807dbbf470d57a01be741
TranDangHoangAnh <70934104+TranDangHoangAnh@users.noreply.github.com>
2020-11-30 11:54:07 +0700

    Update splashsreen

--- TutteeFrame/View/frmSpashScreen.cs
+++ TutteeFrame/View/frmSpashScreen.cs
@@ -7,41 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using TutteeFrame.Model;
+using WMPLib;
 
 namespace TutteeFrame
 {
     public partial class frmSpashScreen : Form
     {
 
-        int second = 2;
-
+        int secondShowing = 2;
         public frmSpashScreen()
         {
             InitializeComponent();
-            Helper.SettingCheck();
+            Controller.Instance.SettingCheck();            
             timer1.Start();
-
         }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (second == 0)
+            if (secondShowing == 0)
             {
                 timer1.Stop();
                 this.Close();
-
             }
             else
-                second--;
-
-
-
+                secondShowing--;
         }
-
         private void SplashScreen_Shown(object sender, EventArgs e)
         {
             this.Activate();
+            WindowsMediaPlayer intro = new WindowsMediaPlayer();
+            intro.URL = @"intro.wav";
+            intro.controls.play();
         }
 
+      
     }
 }
